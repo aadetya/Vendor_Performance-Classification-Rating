1c6093a8a9beccdebfa4fd2bd1c0759db2fbbd70
Anant Verma <anantverma275@yahoo.com>
2020-07-02 18:59:29 +0530

    added comments, improved readability

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def prediction():
     if q_return > qr:
         return ("Invalid input: Quantity returned cannot be greater than quantity received")
     
-    #scaling for promptness
+    # scaling for promptness
     
     daydiff = (pd.to_datetime(de) - pd.to_datetime(dr)).days
     if daydiff > 0:
@@ -36,7 +36,7 @@ def prediction():
     file = file.append({'DeliveryTime': daydiff, 'EnteredReceivedQuantity': qr, 'QuantityDemandedFinal': qd}, ignore_index = True)
     promptness = (daydiff - np.min(file['DeliveryTime']))/(np.max(file['DeliveryTime']) - np.min(file['DeliveryTime']))
     
-    #scaling for quantity
+    # scaling for quantity
     
     min = 1
     max = np.max(file['EnteredReceivedQuantity']/file['QuantityDemandedFinal'])
@@ -45,47 +45,37 @@ def prediction():
     else:
         quantity = 1-(((qr/qd)-min)/(max-min))
     
-    #scaling for quality and check for quantity received is 0
+    # scaling for quality 
     
     try:
         quality = 1 - q_return/qr
     except ZeroDivisionError:
         quality = 1
    
+    # rating for the vendor
+    
     if qr != 0:
         final_score = round((0.5*quality + 0.3*promptness + 0.2*quantity)*5,2)  # Rating of the vendor out 5; weights 0.5,0.3,0.2 for quality, qty, promptness
     else: # quality doesn't make sense if quantity received is zero
         final_score = round((0.55*promptness + 0.45*quantity)*5, 2)
-    
-    #settings the labels correctly
-    # label_dict = {}
-    # test = [[[1,1,1]],[[0,1,1]],[[1,0,1]],[[1,1,0]]]
-    # label_dict['Performing'] = clf.predict(test[0])[0]
-    # label_dict['quantity'] = clf.predict(test[1])[0]
-    # label_dict['promptness'] = clf.predict(test[2])[0]
-    # label_dict['quality'] = clf.predict(test[3])[0]
-    
-    #probablitlites for quntity, promptness and quality of given input
 
-    # result = clf.predict_proba([[quantity, promptness, quality]])[0]
-    
-    #new = [['Quantity', round(result[label_dict['quantity']], 2)], ['Promptness', round(result[label_dict['promptness']], 2)], ['Quality', round(result[label_dict['quality']], 2)]]
+    # final values
     new = [['Quantity', round(quantity, 2)], ['Promptness', round(promptness, 2)], ['Quality', round(quality, 2)]]
+    
+    
     if qr == 0:
-        new.pop()
+        new.pop() # removing quality if received qty is 0
         
     if clf.predict([[quantity, promptness, quality]]) == clf.predict([[1,1,1]]):
         answer = "Performing"
-        new.sort(key = lambda x: x[1], reverse = True)
+        new.sort(key = lambda x: x[1], reverse = True)  # sort by excelling values (in the descending order)
     else:
         answer = "Non-performing"
         if qr == 0:
-            new[0][1] = 0.0
-        new.sort(key = lambda x: x[1])
+            new[0][1] = 0.0             # if receivedqty is zero quantity factor is bad ==> 0
+        new.sort(key = lambda x: x[1])  # sort by most lacking values (in the ascending order)
 
-    # print(quantity, promptness, quality)
-    # print(*result)
-    print(new)
+    print(new) # just to check what's up
     
     return render_template("result.html", answer = answer, value = new, rating = str(final_score), length = len(new))
 
